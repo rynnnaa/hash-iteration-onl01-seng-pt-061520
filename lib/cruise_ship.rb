985cc82af05700778df3fc8d66a376af6506c5c3
Ryna Truong <rynatruong@outlook.com>
2020-06-27 23:10:53 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,8 +8,8 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each |suite, name|
-  if suite == :suite_1 && name.start_with?("A")
+  passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
